6cadaad50cf33283ab6df4f2082bf51adc2f66ee
Regina Liu <62209188+regexyl@users.noreply.github.com>
2023-12-10 14:44:08 +0100

    Delete day01a directory

--- day01a/target/debug/deps/day01a-7ebb2f66ea12de21.d
+++
@@ -1,6 +0,0 @@
-/Users/reginaliu/code/adventofcode-2023/day01a/target/debug/deps/libday01a-7ebb2f66ea12de21.rmeta: src/main.rs src/../input.txt
-
-/Users/reginaliu/code/adventofcode-2023/day01a/target/debug/deps/day01a-7ebb2f66ea12de21.d: src/main.rs src/../input.txt
-
-src/main.rs:
-src/../input.txt:
--- day01a/target/debug/deps/day01a-dd54cbbd4f57f3f5.d
+++
@@ -1,6 +0,0 @@
-/Users/reginaliu/code/adventofcode-2023/day01a/target/debug/deps/day01a-dd54cbbd4f57f3f5: src/main.rs src/../input.txt
-
-/Users/reginaliu/code/adventofcode-2023/day01a/target/debug/deps/day01a-dd54cbbd4f57f3f5.d: src/main.rs src/../input.txt
-
-src/main.rs:
-src/../input.txt:
